48be8373befca57129271961db758328644958f2
AmielCaptainTower <amiel@captaintower.com>
2022-02-19 15:46:59 +0100

    Ajout fond couleur body

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
+    <div  style={{backgroundColor: '#0FDDFC'}}> 
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>} />
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
+    </div >
   );
 };
 
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,22 +5,16 @@ import Formulaire from '../components/Formulaire';
 
 
 const NotFound = () => {
-
-
-
-    
     return (
-
-        
         <div>
             <Navigation />
-            <Logo/>
+            <Logo />
             <h1>Submit new coin to Fantom Tribune</h1>
-<br />
-<br />
-<br />
+            <br />
+            <br />
+            <br />
 
-            <Formulaire/>
+            <Formulaire />
         </div>
     );
 };
