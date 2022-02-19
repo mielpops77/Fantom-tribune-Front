065ea5d6558da13ec1f9047cb638a3cc62acc245
AmielCaptainTower <amiel@captaintower.com>
2022-02-19 13:23:55 +0100

    add fond header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import background from "./img/fond_bandeau.png";
 
 const Navigation = () => {
   return (
-    <nav className="navigation">
-      <ul>
-        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-          <li>HOME</li>
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={(nav) => (nav.isActive ? "nav-active" : "")}
-        >
-          <li>LAUNCH DATE</li>
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={(nav) => (nav.isActive ? "nav-active" : "")}
-        >
-          <li>SUBMIT PROJECT</li>
-        </NavLink>
-        <NavLink
-          to="/connexion"
-          className={(nav) => (nav.isActive ? "nav-active" : "")}
-        >
-          <li>NEWS</li>
-        </NavLink>
-      </ul>
-    </nav>
+    // eslint-disable-next-line react/style-prop-object
+    
+
+      <nav className="navigation" style={{backgroundImage: `url(${background})`}}>
+        <ul>
+          <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+            <li>HOME</li>
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>LAUNCH DATE</li>
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>SUBMIT PROJECT</li>
+          </NavLink>
+          <NavLink
+            to="/connexion"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>NEWS</li>
+          </NavLink>
+        </ul>
+      </nav>
+
+
   );
 };
 
