8cd9f879f695b949dffbf88c752a5daf76e3d404
mielpops77 <amielzeitoun@gmail.com>
2022-03-10 16:08:44 +0100

    edit tableau launch

--- src/components/user/TableauLaunch.component.js
+++ src/components/user/TableauLaunch.component.js
@@ -1,26 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MDBDataTableV5 } from 'mdbreact';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
+
 
 const TableauLaunch = () => {
+
 
   const [posts, setPosts] = useState([]);
   var [totalReactPackages, setTotalReactPackages] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/launchDate/')
-        .then((res) => res.json())
-        .then((res) => {
-            setPosts(res);
-        });
-}, []);
+      .then((res) => res.json())
+      .then((res) => {
+        setPosts(res);
+      });
+  }, []);
 
 
 
   useEffect(() => {
     let postsArray = JSON.parse(JSON.stringify(posts));
     let userData = [];
+    console.log('postsArray', postsArray);
     postsArray.map((item, index) => {
       item._id = (
         <div style={{ fontWeight: "bold", fontSize: "1.2em" }}>{item._id}</div>
@@ -37,8 +40,10 @@ const TableauLaunch = () => {
   }, [posts]);
 
 
-
-
+  function test() {
+    console.log(data.row)
+    return data.rows;
+  }
 
   const data = {
     columns: [
@@ -74,33 +79,77 @@ const TableauLaunch = () => {
       },
 
     ],
-    rows: totalReactPackages
+    rows: [
+    ]
   };
 
 
-  /*   if (totalReactPackages != null) {
-      console.log(data);
-      console.log(totalReactPackages);
-  
-      for (let i = 0; i < totalReactPackages.length; i++) {
-        data.rows.push(({ image: <img style={{ height: "100%", width: "95px", float: "left" }} src={"http://localhost:3000/" + totalReactPackages[i].image} />, name: totalReactPackages[i].name, symbol: totalReactPackages[i].symbol, launchDate: totalReactPackages[i].launchDate }));
-      }
-  
+  if (totalReactPackages != null) {
+    console.log(data);
+    console.log(totalReactPackages);
+
+    for (let i = 0; i < totalReactPackages.length; i++) {
+      data.rows.push(({ image: <img style={{ height: "100%", width: "95px", float: "left" }} src={totalReactPackages[i].image.props.src} />, name: totalReactPackages[i].name, symbol: totalReactPackages[i].symbol, launchDate: totalReactPackages[i].launchDate, }));
     }
-   */
+
+  }
 
 
+
+  let id = 0;
+  function createData(option, type) {
+    id += 1;
+    return { id, option, type };
+  }
+
+  
+  let rows = [
+    createData('Setting Two', 'Public'),
+    createData('Setting Three', 'Group'),
+    createData('Setting Four', 'Private'),
+  ];
+
+  console.log('rows',rows);
+  console.log('rows',data.rows);
+
+  
+  
 
   return (
     <div className="container">
-      <MDBDataTableV5
+
+<table class="table table-striped table-hover">
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col"> </th>
+      <th scope="col">Name</th>
+      <th scope="col">Symbol</th>
+      <th scope="col">LaunchDate</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+  {data.rows.map(row => (
+        <tr>
+          <td></td>
+          <td><img style={{ height: "100px", width: "100px" }} src={row.image.props.src} /></td>
+          <td>{row.name}</td>
+          <td>{row.symbol}</td>
+          <td>{row.launchDate}</td>
+        </tr>
+      ))}
+  </tbody>
+</table>
+    {/*   <MDBDataTableV5
         responsive
         hover
         striped
         bordered
         small
-        data={data}
-      />
+        columns={data.columns}
+        rows = {test()}
+      /> */}
     </div>
   );
 }
