c41dad68f14b9ee18166a88ea503989e560fb09b
mielpops77 <amielzeitoun@gmail.com>
2022-09-28 19:01:41 +0200

    Top trending update
    Mise en place du titre

--- src/components/User/TopTrending/TopTrending.component.js
+++ src/components/User/TopTrending/TopTrending.component.js
@@ -1,16 +1,23 @@
 import NavigationUserComponent from '../../Navigation/NavigationUser/NavigationUser.component';
-import style from "./TopTrending.module.scss";
+import AuthService from "../../../services/auth/auth.service";
+import style from './TopTrending.module.scss';
+
 import React from 'react';
 
 const TopTrending = () => {
+  const url = AuthService.getUrl();
+
   return (
-    <div>
-       <NavigationUserComponent/>
+    <div className={style.topTrending_mainDiv}>
+      <NavigationUserComponent />
+      <div className={style.topTrending_divSectionTitle}>
+        <img src={url + "assets/ranked_arrows.png"} className={style.topTrending_imgLogoSection} alt='ranked_arrow'></img>
+        <p className={style.topTrending_sectionTitle}>Top Ranked <span className={style.topTrending_tokensTitle}>Tokens</span> </p>
+      </div>
     </div>
 
   )
 }
-
 
 export default TopTrending;
 
