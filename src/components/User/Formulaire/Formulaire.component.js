938020e401b7ab230f82f02160ed201473717315
mielpops77 <amielzeitoun@gmail.com>
2022-11-09 21:30:18 +0100

    Restriction User non connecté pour pas qu'ils puissent soumettre un new projet

--- src/components/User/Formulaire/Formulaire.component.js
+++ src/components/User/Formulaire/Formulaire.component.js
@@ -3,9 +3,11 @@ import AuthService from "../../../services/auth/auth.service";
 import { useNavigate } from 'react-router-dom';
 import style from "./Formulaire.module.scss";
 import Select from 'react-select'
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function Formulaire() {
 
@@ -17,8 +19,24 @@ function Formulaire() {
   const [prev, setPrev] = useState('');
   const [kyc, setKyc] = useState('');
   const [verifUpl, setVerifUpl] = useState('');
+  const [user, setUser] = useState([]);
 
   const navigate = useNavigate();
+
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+
+
+
+  useEffect(() => {
+    setUser(AuthService.getCurrentUser());
+    console.log('user', user);
+  }, [])
+
 
   function nav(path) {
     navigate(path);
@@ -27,6 +45,30 @@ function Formulaire() {
   if (urlUpload !== '' && !verifUpl) {
     setVerifUpl(true);
   }
+
+
+
+
+
+
+  function login() {
+    // history.push(`/login/`)
+    navigate(`/login/`);
+  }
+
+  const styleBox = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '30%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
 
   let date = new Date();
   let mondayUtc = (date.getUTCMonth() + 1)
@@ -271,53 +313,55 @@ function Formulaire() {
 
 
   const handleSubmit = (event) => {
-    let coinMarketCapLink;
-    let coinMarketCapStatus;
-    if (inputs.coinMarketCapLink === undefined) {
-      coinMarketCapLink = 'none';
-      coinMarketCapStatus = 'none';
 
-    }
-    else {
-      coinMarketCapStatus = "en cours de validation"
-      const searchTerm = '/currencies/'
-      const slug = inputs.coinMarketCapLink.substring(inputs.coinMarketCapLink.lastIndexOf(searchTerm) + 12, inputs.coinMarketCapLink.length - 1)
-      coinMarketCapLink = inputs.coinMarketCapLink;
-      TableLaunchService.coinmarketCap(slug, coinMarketCapLink);
-    }
-    if (inputs.launchDate === undefined) {
-      if (prev === 'yes') {
-        inputs.launchDate = dateUtc;
+    if (user !== null) {
+      let coinMarketCapLink;
+      let coinMarketCapStatus;
+      if (inputs.coinMarketCapLink === undefined) {
+        coinMarketCapLink = 'none';
+        coinMarketCapStatus = 'none';
+
       }
-      if (prev === 'no') {
-        inputs.launchDate = dateUtcMax;
+      else {
+        coinMarketCapStatus = "en cours de validation"
+        const searchTerm = '/currencies/'
+        const slug = inputs.coinMarketCapLink.substring(inputs.coinMarketCapLink.lastIndexOf(searchTerm) + 12, inputs.coinMarketCapLink.length - 1)
+        coinMarketCapLink = inputs.coinMarketCapLink;
+        TableLaunchService.coinmarketCap(slug, coinMarketCapLink);
       }
+      if (inputs.launchDate === undefined) {
+        if (prev === 'yes') {
+          inputs.launchDate = dateUtc;
+        }
+        if (prev === 'no') {
+          inputs.launchDate = dateUtcMax;
+        }
+      }
+
+
+
+      if (selected === null || selected === 'Dex') {
+        type = 'Dex'
+      }
+      else {
+        type = selected.value;
+      }
+      event.preventDefault();
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: inputs.name, symbol: inputs.symbol, launchDate: inputs.launchDate, contractAddress: inputs.contractAddress.toLowerCase(), description: inputs.description, type: type,
+          websiteLink: inputs.websiteLink, coinMarketCapLink: coinMarketCapLink, telegram: inputs.telegram, twitter: inputs.twitter, discord: inputs.discord, image: inputs.image, vote: 0, voteToday: voteTodayUtc, voteTwentyHour: 0, voteTwentyHourCalcul: voteTwentyHourCalcul, price: 0, marketCap: 0, supply: 0, coinMarketCapStatus: coinMarketCapStatus, idCoinMarketCap: 0, listePriceIdCoinMarketCap: listePriceIdCoinMarketCap, percent_change_24h: 0, promotedStatus: false, kyc: kyc
+        })
+      };
+      fetch(url + 'launchDate', requestOptions)
+        .then(response => response.json(), nav(`/ValidationForm/Submit`)
+        )
+
+      /* .then(data => this.setState({ postId: data.id })); */
+
     }
-
-
-
-    if (selected === null || selected === 'Dex') {
-      type = 'Dex'
-    }
-    else {
-      type = selected.value;
-    }
-    event.preventDefault();
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        name: inputs.name, symbol: inputs.symbol, launchDate: inputs.launchDate, contractAddress: inputs.contractAddress.toLowerCase(), description: inputs.description, type: type,
-        websiteLink: inputs.websiteLink, coinMarketCapLink: coinMarketCapLink, telegram: inputs.telegram, twitter: inputs.twitter, discord: inputs.discord, image: inputs.image, vote: 0, voteToday: voteTodayUtc, voteTwentyHour: 0, voteTwentyHourCalcul: voteTwentyHourCalcul, price: 0, marketCap: 0, supply: 0, coinMarketCapStatus: coinMarketCapStatus, idCoinMarketCap: 0, listePriceIdCoinMarketCap: listePriceIdCoinMarketCap, percent_change_24h: 0, promotedStatus: false, kyc: kyc
-      })
-    };
-    fetch(url + 'launchDate', requestOptions)
-      .then(response => response.json(), nav(`/ValidationForm/Submit`)
-      )
-
-    /* .then(data => this.setState({ postId: data.id })); */
-
-
 
   }
 
@@ -333,123 +377,124 @@ function Formulaire() {
 
   return (
 
-    <form className={style.formulaireSubmit} onSubmit={handleSubmit}>
-      {
-        verifUpl === false && <div className={style.messageError} style={{ marginTop: "1rem" }}>
-          Please upload the logo for your coin.
-        </div>}
+    <div>
+      <form className={style.formulaireSubmit} onSubmit={handleSubmit}>
+        {
+          verifUpl === false && <div className={style.messageError} style={{ marginTop: "1rem" }}>
+            Please upload the logo for your coin.
+          </div>}
 
-      <label className={style.formLabelFileEmpty} htmlFor="file-input">
-        <div className={style.formLabel}>Logo Upload*</div>
-        <img alt='img' style={{ height: "100%", float: "left", maxWidth: "30%", maxHeight: "30%", cursor: 'pointer' }} src={url + "assets/upload.png"} />
-        {urlUpload !== '' && <img alt='img' style={{ height: "100%", float: "left", maxWidth: "25%", maxHeight: "25%" }} src={urlUpload} />}
-      </label>
+        <label className={style.formLabelFileEmpty} htmlFor="file-input">
+          <div className={style.formLabel}>Logo Upload*</div>
+          <img alt='img' style={{ height: "100%", float: "left", maxWidth: "30%", maxHeight: "30%", cursor: 'pointer' }} src={url + "assets/upload.png"} />
+          {urlUpload !== '' && <img alt='img' style={{ height: "100%", float: "left", maxWidth: "25%", maxHeight: "25%" }} src={urlUpload} />}
+        </label>
 
-      <input id="file-input" className={style.file} type="file" name="image" value={inputs.image || ""}
-        onChange={handleChangeFile}
-        accept="image/png, image/jpeg"
-        required="required"
-      >
-      </input>
-
-      <label className={style.formLabel}>Name*:
-        <input className={style.formInput}
-          type="text"
-          name="name"
-          value={inputs.name || ""}
-          onChange={handleChange}
+        <input id="file-input" className={style.file} type="file" name="image" value={inputs.image || ""}
+          onChange={handleChangeFile}
+          accept="image/png, image/jpeg"
           required="required"
-        />
-      </label>
-      <label className={style.formLabel}>Symbol*:
-        <input className={style.formInput}
-          type="text"
-          name="symbol"
-          value={inputs.symbol || ""}
-          onChange={handleChange}
-          required="required"
-        ></input>
-      </label>
+        >
+        </input>
 
-
-
-
-
-      <label className={style.formLabel}>Project in the launch phase?*:
-        <div>
-
-          <input onChange={prevente} type="radio" name="question" value="yes" id="yes" required="required"
-          /> <label style={{ marginRight: "1%" }} htmlFor="yes">yes</label>
-          <input onChange={prevente} type="radio" name="question" value="no" id="no" required="required"
-          /> <label htmlFor="no">no</label>
-        </div>
-      </label>
-
-      {
-        prev === "yes" && <label className={style.formLabel}>LaunchDate (UTC)*:
+        <label className={style.formLabel}>Name*:
           <input className={style.formInput}
-            type="date"
-            name="launchDate"
-            min={dateUtc}
-            value={inputs.launchDate || dateUtc}
+            type="text"
+            name="name"
+            value={inputs.name || ""}
             onChange={handleChange}
+            required="required"
           />
         </label>
-      }
-
-      {
-        prev === "no" && <label className={style.formLabel}>LaunchDate (UTC)*:
+        <label className={style.formLabel}>Symbol*:
           <input className={style.formInput}
-            type="date"
-            name="launchDate"
-            max={dateUtcMax}
-            value={inputs.launchDate || dateUtcMax}
+            type="text"
+            name="symbol"
+            value={inputs.symbol || ""}
+            onChange={handleChange}
+            required="required"
+          ></input>
+        </label>
+
+
+
+
+
+        <label className={style.formLabel}>Project in the launch phase?*:
+          <div>
+
+            <input onChange={prevente} type="radio" name="question" value="yes" id="yes" required="required"
+            /> <label style={{ marginRight: "1%" }} htmlFor="yes">yes</label>
+            <input onChange={prevente} type="radio" name="question" value="no" id="no" required="required"
+            /> <label htmlFor="no">no</label>
+          </div>
+        </label>
+
+        {
+          prev === "yes" && <label className={style.formLabel}>LaunchDate (UTC)*:
+            <input className={style.formInput}
+              type="date"
+              name="launchDate"
+              min={dateUtc}
+              value={inputs.launchDate || dateUtc}
+              onChange={handleChange}
+            />
+          </label>
+        }
+
+        {
+          prev === "no" && <label className={style.formLabel}>LaunchDate (UTC)*:
+            <input className={style.formInput}
+              type="date"
+              name="launchDate"
+              max={dateUtcMax}
+              value={inputs.launchDate || dateUtcMax}
+              onChange={handleChange}
+            />
+          </label>
+        }
+
+
+        <label className={style.formLabel}>Contract Address:
+          <input className={style.formInput}
+            type="text"
+            name="contractAddress"
+            value={inputs.contractAddress || ""}
             onChange={handleChange}
           />
+
         </label>
-      }
 
+        <label className={style.formLabel}>Description*:
+          <textarea className={style.formInput} style={{
+            maxHeight: "40em",
+            minHeight: "8em"
+          }}
+            type="text"
+            name="description"
+            value={inputs.description || ""}
+            onChange={handleChange}
+            required="required"
+          />
+        </label>
 
-      <label className={style.formLabel}>Contract Address:
-        <input className={style.formInput}
-          type="text"
-          name="contractAddress"
-          value={inputs.contractAddress || ""}
-          onChange={handleChange}
-        />
+        <label className={style.formLabel}>Type*:
+          <Select
+            className="basic-single"
+            classNamePrefix="select"
+            /* defaultValue={options[0]} */
+            /*  isClearable={true} */
+            defaultValue={options[0]}
+            isSearchable={true}
+            options={options}
+            /* value={selected} */
+            onChange={setSelected}
+            selectOption="required"
+          />
 
-      </label>
+        </label>
 
-      <label className={style.formLabel}>Description*:
-        <textarea className={style.formInput} style={{
-          maxHeight: "40em",
-          minHeight: "8em"
-        }}
-          type="text"
-          name="description"
-          value={inputs.description || ""}
-          onChange={handleChange}
-          required="required"
-        />
-      </label>
-
-      <label className={style.formLabel}>Type*:
-        <Select
-          className="basic-single"
-          classNamePrefix="select"
-          /* defaultValue={options[0]} */
-          /*  isClearable={true} */
-          defaultValue={options[0]}
-          isSearchable={true}
-          options={options}
-          /* value={selected} */
-          onChange={setSelected}
-          selectOption="required"
-        />
-
-      </label>
-
-      {/*     <label className={style.formLabel}>Type*:
+        {/*     <label className={style.formLabel}>Type*:
         <MultiSelect
           className={style.multiSelect}
           options={options}
@@ -462,64 +507,84 @@ function Formulaire() {
 
       </label>
  */}
-      <label className={style.formLabel}>Website link*:
-        <input className={style.formInput}
-          type="text"
-          name="websiteLink"
-          value={inputs.websiteLink || ""}
-          onChange={handleChange}
-          required="required"
-        />
-      </label>
+        <label className={style.formLabel}>Website link*:
+          <input className={style.formInput}
+            type="text"
+            name="websiteLink"
+            value={inputs.websiteLink || ""}
+            onChange={handleChange}
+            required="required"
+          />
+        </label>
 
-      <label className={style.formLabel}>coinmarketcap link:
-        <input className={style.formInput}
-          type="text"
-          name="coinMarketCapLink"
-          value={inputs.coinMarketCapLink || ""}
-          onChange={handleChange} />
-      </label>
+        <label className={style.formLabel}>coinmarketcap link:
+          <input className={style.formInput}
+            type="text"
+            name="coinMarketCapLink"
+            value={inputs.coinMarketCapLink || ""}
+            onChange={handleChange} />
+        </label>
 
-      <label className={style.formLabel}>Telegram link:
-        <input className={style.formInput}
-          type="text"
-          name="telegram"
-          value={inputs.telegram || ""}
-          onChange={handleChange}>
-        </input>
-      </label>
-
-
-      <label className={style.formLabel}>Twitter link*:
-        <input className={style.formInput}
-          type="text"
-          name="twitter"
-          value={inputs.twitter || ""}
-          onChange={handleChange}
-          required="required" />
-      </label>
+        <label className={style.formLabel}>Telegram link:
+          <input className={style.formInput}
+            type="text"
+            name="telegram"
+            value={inputs.telegram || ""}
+            onChange={handleChange}>
+          </input>
+        </label>
 
 
-      <label className={style.formLabel}>Discord link:
-        <input className={style.formInput}
-          type="text"
-          name="discord"
-          value={inputs.discord || ""}
-          onChange={handleChange} />
-      </label>
-      <label className={style.formLabel}>Kyc? *:
-        <div>
-          <input onChange={kycEdit} type="radio" name="questionKyc" value="yes" id="yes" required="required"
-          /> <label style={{ marginRight: "1%" }} htmlFor="yes">yes</label>
-          <input onChange={kycEdit} type="radio" name="questionKyc" value="no" id="no" required="required"
-          /> <label htmlFor="no">no</label>
-        </div>
-      </label>
-      <br />
+        <label className={style.formLabel}>Twitter link*:
+          <input className={style.formInput}
+            type="text"
+            name="twitter"
+            value={inputs.twitter || ""}
+            onChange={handleChange}
+            required="required" />
+        </label>
 
-      <input className={style.blueButton} type="submit" onClick={verifUpload} />
-    </form >
+
+        <label className={style.formLabel}>Discord link:
+          <input className={style.formInput}
+            type="text"
+            name="discord"
+            value={inputs.discord || ""}
+            onChange={handleChange} />
+        </label>
+        <label className={style.formLabel}>Kyc? *:
+          <div>
+            <input onChange={kycEdit} type="radio" name="questionKyc" value="yes" id="yes" required="required"
+            /> <label style={{ marginRight: "1%" }} htmlFor="yes">yes</label>
+            <input onChange={kycEdit} type="radio" name="questionKyc" value="no" id="no" required="required"
+            /> <label htmlFor="no">no</label>
+          </div>
+        </label>
+        <br />
+
+        <input className={style.blueButton} type="submit" onClick={verifUpload} />
+      </form >
+
+
+
+
+      <Modal
+        open={user == null}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={styleBox}>
+          <Typography className={style.typo} id="modal-modal-title" variant="h6" component="h2">
+            You must be logged in to be able to submit a new project
+          </Typography>
+          <br />
+          <button style={{ width: "100%" }} className="btn btn-success" onClick={login}>login</button>
+        </Box>
+      </Modal>
+    </div>
   )
+
 }
 
 /* ReactDOM.render(<Formulaire />, document.getElementById('root')); */
