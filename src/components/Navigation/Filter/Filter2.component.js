97f9c7969e739aed3ed90023dfa48bd7b5bd6db9
mielpops77 <amielzeitoun@gmail.com>
2022-12-05 21:01:56 +0100

    Optimisation de SearchAutocomplete

--- src/components/Navigation/Filter/Filter2.component.js
+++ src/components/Navigation/Filter/Filter2.component.js
@@ -8,25 +8,56 @@ import axios from "axios";
 
 const Filter2 = () => {
 
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState([
+        {
+            id: 0,
+            name: 'vide',
+            image: 'vide',
+            symbol: 'vide',
+            type: 'vide'
+        }]);
     const url = AuthService.getUrl();
     const navigate = useNavigate();
 
+    let b = [
+        {
+            id: 0,
+            name: 'vide',
+            image: 'vide',
+            symbol: 'vide',
+            type: 'vide'
+        }];
+
+    const items2 = [
+        {
+            id: 0,
+            name: "aaaaa",
+        },
+        {
+            id: 1,
+            name: "aaaaaaaaa",
+        },
+
+    ];
+
+
     const handleOnSearch = (string, results) => {
-        getSearchCoinRequest(string);
+        console.log('results', results);
+        // getSearchCoinRequest(string);
     }
 
+
     const handleOnSelect = (item) => {
-        console.log("item", item);
         navigate(`/infoCoin/${item.id}`);
     }
 
 
 
     const formatResult = (item) => {
+
         return (
             <>
-                <span className={style.filter_search}>  <img className={style.filter_img} src={url + item.image} alt='img' />  <span className={style.filter_nameSearch}>{item.name}</span> 	<mat-chip>
+                <span className={style.filter_search}><img className={style.filter_img} src={url + item.image} alt='img' />  <span className={style.filter_nameSearch}>{item.name}</span> 	<mat-chip>
                     <label htmlFor="chip-1">{item.symbol}</label>
                 </mat-chip></span>
             </>
@@ -38,6 +69,10 @@ const Filter2 = () => {
 
     }
 
+    function test(event) {
+        console.log("event:", event.target.value)
+        getSearchCoinRequest(event.target.value);
+    }
     function getSearchCoinRequest(search) {
         return axios.get(AuthService.getUrl() + `searchCoin?name=${search}`)
             .then(response => {
@@ -129,13 +164,9 @@ const Filter2 = () => {
                     result[i].image = response.data[i].image;
                     result[i].symbol = response.data[i].symbol;
                     result[i].type = response.data[i].type;
-
                 }
-
                 const result2 = result.filter((person) => person.name !== 'vide')
                 setItems(result2);
-
-
                 return response.data;
             })
     }
@@ -143,26 +174,43 @@ const Filter2 = () => {
         <div className={style.searchFilter}>
             <div className={style.filter}>
 
-                <ReactSearchAutocomplete
-                    styling={
-                        {
+                <div onChange={test}>
+                    <ReactSearchAutocomplete
+                        styling={
+                            {
 
-                            border: '1px solid #000',
-                            fontSize: "40px",
-                            height: "135px !IMPORTANT",
-                            fontFamily: "arial",
-                            fill: "blue !IMPORTANT",
+                                border: '1px solid #000',
+                                fontSize: "40px",
+                                height: "135px !IMPORTANT",
+                                fontFamily: "arial",
+                                fill: "blue !IMPORTANT",
+                            }
                         }
-                    }
-                    placeholder="Search..."
-                    items={items}
-                    onSearch={handleOnSearch}
-                    onSelect={handleOnSelect}
-                    autoFocus
-                    formatResult={formatResult}
-                />
+                        fuseOptions={
+                            {
+                                shouldSort: true,
+                                threshold: 0.6,
+                                location: 0,
+                                distance: 100,
+                                maxPatternLength: 32,
+                                minMatchCharLength: 1,
+                                keys: [
+                                    "name", "symbol"
+                                ]
+                            }
+                        }
+                        resultStringKeyName="name"
 
+                        placeholder="Search..."
+                        items={items}
+                        onSearch={handleOnSearch}
+                        onSelect={handleOnSelect}
+                        // fuseOptions={{ keys: ["title", "description"] }}
+                        /* autoFocus */
+                        formatResult={formatResult}
+                    />
 
+                </div>
 
             </div>
             <button onClick={searchButton} className={style.searchButton}>
--- src/components/User/UpdateCoin/UpdateCoin.component.js
+++ src/components/User/UpdateCoin/UpdateCoin.component.js
@@ -491,6 +491,20 @@ const UpdateCoin = () => {
                     onFocus={handleOnFocus}
                     autoFocus
                     formatResult={formatResult}
+                    fuseOptions={
+                        {
+                            shouldSort: true,
+                            threshold: 0.6,
+                            location: 0,
+                            distance: 100,
+                            maxPatternLength: 32,
+                            minMatchCharLength: 1,
+                            keys: [
+                                "name", "symbol"
+                            ]
+                        }
+                    }
+                    resultStringKeyName="name"
                 />
             </div>
 
